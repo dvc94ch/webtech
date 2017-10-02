5cf1539caeebbd32e3059d942c3379de4b5d2028
David Craven <david@craven.ch>
2017-10-02 12:00:01 +0200

    Initial commit.

---
+++ env.sh
@@ -0,0 +1,3 @@
+export RUSTFLAGS="-L $HOME/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/x86_64-unknown-linux-gnu/lib/"
+export GUIX_LD_WRAPPER_ALLOW_IMPURITIES=y
+export CC=/home/dvc/.guix-profile/bin/gcc
