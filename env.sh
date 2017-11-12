22fb5758cf9f04d5d4e7b41cd782d7420f71f10d
David Craven <david@craven.ch>
2017-11-12 15:59:25 +0100

    WIP steed docker deployment.

--- env.sh
+++ env.sh
@@ -1,3 +1,4 @@
-export RUSTFLAGS="-L $HOME/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/x86_64-unknown-linux-gnu/lib/"
 export GUIX_LD_WRAPPER_ALLOW_IMPURITIES=y
-export CC=/home/dvc/.guix-profile/bin/gcc
+export RUST_BACKTRACE=1
+export CC=$HOME/.guix-profile/bin/gcc
+export PATH=$HOME/.cargo/bin:$PATH
